fb128ef906a9dd7ed869d39e8c55c269cfc8ede1
LimitToInfinity <21stLimitless@gmail.com>
2020-10-27 16:56:53 -0600

    Adds free brands and free regular font awesome icons.

--- src/components/TvShowCard.js
+++ src/components/TvShowCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/fontawesome-svg-core';
+
 function TvShowCard({ tvShow }) {
   return (
     <div className="tv-show-card">
       <h3>{tvShow.name}</h3>
       <img src={tvShow.image.medium} alt={tvShow.name} />
-      <button><i class="far fa-star"></i></button>
+      <button><FontAwesomeIcon icon={faStar} /></button>
     </div>
 
   );
